c6f55112f46d5e3730d133c68bc3179f96a8459e
jeekpark <jeekpark@student.42seoul.kr>
2024-10-18 21:43:47 +0900

    Feat: delta_time var for update

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,5 @@
 use ft_vox_prototype_0_map_core::Map;
-use std::{cell::RefCell, marker::PhantomData, num::NonZeroU8, rc::Rc, sync::Arc};
+use std::{cell::RefCell, marker::PhantomData, num::NonZeroU8, rc::Rc, sync::Arc, time::Instant};
 use winit::{
     event::{Event, KeyEvent, WindowEvent},
     event_loop::{EventLoop, EventLoopWindowTarget},
@@ -132,7 +132,7 @@ pub async fn run() {
     let mut frame_driven_input = FrameDrivenInput::new();
 
     let event_loop_function = EventLoop::run;
-
+    let mut last_frame_time = Instant::now();
     #[allow(clippy::let_unit_value)]
     let _ = (event_loop_function)(
         window_loop.event_loop,
@@ -201,6 +201,9 @@ pub async fn run() {
                         // If this happens, just drop the requested redraw on the floor.
                         //
                         // See https://github.com/rust-windowing/winit/issues/3235 for some discussion
+                        let now = Instant::now();
+                        let delta_time = now.duration_since(last_frame_time).as_secs_f32();
+                        last_frame_time = now;
 
                         frame_driven_input.update(&event_driven_input);
 
@@ -212,8 +215,8 @@ pub async fn run() {
                                         window_loop.window.inner_size(),
                                     );
                                 }
-                                vox.update_eye_movement(&frame_driven_input);
-                                vox.update_eye_rotation(&frame_driven_input, target);
+                                vox.update_eye_movement(delta_time, &frame_driven_input);
+                                vox.update_eye_rotation(delta_time, &frame_driven_input, target);
                                 vox.update_nearby_chunks(&context);
 
                                 if vox.is_paused {
