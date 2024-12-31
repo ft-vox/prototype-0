57cc0114d36a50e9c500a769a417e31650888d79
Juyeong Maing <mjy9088@naver.com>
2024-12-31 16:37:46 +0900

    remove external dependency/plugin, change directory structure

--- init_compile_commands.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-echo '[{"directory":"'"$(pwd)"'","arguments":["clang","-Iinclude","-Iexternal_headers","-x","c","-std=c99","-Wall","-Wextra","-Werror","-pedantic","-c","file.c","-o","file.o"],"file":"file.c" }]' > compile_commands.json
